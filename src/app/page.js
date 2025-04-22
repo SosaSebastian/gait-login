53202b91eb412e639d21a607a2dc8c32c7c7bdcc
Sebastian SM <seb.sossa15@gmail.com>
2025-04-21 21:49:47 -0500

    Seccion de inicio de sesion

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,26 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <div className="bg-purple-950 rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
           <div className="w-3/5 p-5"> 
-            <p>login section</p> 
+            <div>LOGO DE GAIT</div>
+            <div className="py-10">
+              <h2 className="text-3xl font-bold text-gray-300 mb-8">Iniciar Sesión</h2>  
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="bg-gray-300 w-64 p-2 flex items-center mb-3">
+                <input type="email" name="email" placeholder="Email" className="bg-gray-300 text-black outline-none text-sm"/>
+              </div>
+            </div> 
+            <div className="flex flex-col items-center">
+              <div className="bg-gray-300 w-64 p-2 flex items-center mb-5">
+                <input type="password" name="password" placeholder="Contraseña" className="bg-gray-300 text-black outline-none text-sm"/>
+              </div>
+            </div>
+            <div className="flex flex-col items-center mb-5">
+              <a href="#" className="">Olvidaste tu Contraseña?</a>
+            </div>
+            <a href="#" className="border-2 border-gray-300 rounded-full px-12 py-2 font-semibold">Iniciar Sesion</a>      
           </div>
+          
           <div className="w-2/5 bg-gray-300 text-black rounded-tr-2xl rounded-br-2xl py-35 px-15"> 
             <h2 className="text-3xl font-bold mb-10">Bienvenido a GAIT</h2>
             <p className="mb-10 ">¿Aún no tienes un usuario? Solicita tus credenciales de registro</p>
